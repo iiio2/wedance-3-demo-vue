c227e89fb66e647113a04ed101757ed9c3eca348
Mr. Robot <gomexxxx71@gmail.com>
2023-04-23 23:54:50 +0600

    initial commit.

---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="about p-4 text-justify text-neutral-950 leading-8 text-lg min-h-screen">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora provident non aliquid, alias
+    quos, odit repellat saepe qui laborum pariatur eos unde rerum asperiores voluptatum, excepturi
+    possimus natus. Ab nam cupiditate unde impedit, excepturi consectetur inventore. Assumenda quos
+    adipisci, aspernatur eaque, repellat facere officia laborum vitae corrupti quis, voluptatem
+    obcaecati?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odit tempore,
+    facilis voluptatibus optio distinctio excepturi sed iste veritatis mollitia! Error rerum
+    nesciunt molestiae iure! Eius ut commodi asperiores error corrupti at. Eaque amet ducimus
+    reiciendis, iste sint omnis nesciunt eos necessitatibus expedita quis earum et voluptatem, nisi
+    enim provident! Aliquam recusandae iure ab sed consequatur ducimus voluptate tempora nobis, qui
+    aliquid quam amet aut repellendus ut ipsa maiores similique aspernatur incidunt! Eos dolorem
+    consequuntur explicabo ratione dolor nesciunt facilis ad libero rem nemo. Doloribus aliquam nisi
+    dolore reprehenderit? Voluptatem dolorem sunt autem similique perferendis. Velit at quae laborum
+    minima?
+  </p>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import home from '../assets/home.jpg'
+</script>
+
+<template>
+  <div class="hero min-h-screen" :style="{ backgroundImage: 'url(' + home + ')' }">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello, WeDance</h1>
+        <p class="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
