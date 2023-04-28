c5844d56a167113b670f0519dd2eaacca2809d1e
Mr. Robot <gomexxxx71@gmail.com>
2023-04-28 12:29:25 +0600

    build spinner.

---
+++ src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const styleObject = reactive({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh'
+})
+</script>
+
+<template>
+  <div :style="styleObject">
+    <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
+      <div
+        class="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-8 h-64 w-64"
+      ></div>
+    </div>
+  </div>
+</template>
---
+++ src/components/WithSpinner.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import useAuth from '@/composables/useAuth'
+import Spinner from './Spinner.vue'
+import NotFound from './NotFound.vue'
+
+const { user, loading } = useAuth()
+</script>
+
+<template>
+  <div v-if="loading">
+    <Spinner />
+  </div>
+  <div v-else-if="!user && !user?.uid">
+    <NotFound />
+  </div>
+  <div v-else>
+    <slot />
+  </div>
+</template>
